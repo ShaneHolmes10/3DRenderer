4b01b4007db0fe0333a35e53d4d43035823f03ec
Shane <shaneholmes2010@gmail.com>
2024-07-02 11:39:34 -0400

    Adding test utils to remote repo

---
+++ libraries/Testing_Utils.h
@@ -0,0 +1,69 @@
+
+
+#ifndef TESTING_UTILS_H
+#define TESTING_UTILS_H
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
+#include <Eigen/Dense>
+#include <iostream>
+
+namespace TestUtils {
+
+int test_counter_testUtils = 1;
+int correct_counter_testUtils = 0;
+
+inline bool check_matrix_3_similarity(const Eigen::Matrix3d& m1, const Eigen::Matrix3d& m2) {
+
+    return ( 0.1 > (m1 - m2).norm()) ;
+}
+
+inline bool check_matrix_4_similarity(const Eigen::Matrix4d& m1, const Eigen::Matrix4d& m2) {
+
+    return ( 0.1 > (m1 - m2).norm()) ;
+}
+
+inline bool check_vector_3_similarity(const Eigen::Vector3d& v1, const Eigen::Vector3d& v2) {
+
+    return ( 0.1 > (v1 - v2).norm()) ;
+}
+
+inline void pass_or_fail_printout(bool value) {
+    if(value) {
+        std::cout << GREEN << "[TEST SUCCESSFUL]" << RESET << std::endl;
+        correct_counter_testUtils++;
+    } else {
+        std::cout << RED << "[TEST FAILED]" << RESET << std::endl;
+
+    }
+}
+
+inline void start_testing() {
+    std::cout << CYAN << "************************************************************************************" << RESET << std::endl;
+    std::cout << BLUE << "[TESTING STARTED]" << RESET << std::endl;
+    std::cout << CYAN << "************************************************************************************" << RESET << std::endl;
+}
+
+inline void perform_test(std::function<void(void)> func) {
+    std::cout << BLUE << "**TEST " << test_counter_testUtils << "**" << RESET << std::endl;
+    func();
+    test_counter_testUtils++;
+}
+
+inline void end_testing() {
+    std::cout << CYAN << "************************************************************************************" << RESET << std::endl;
+    std::cout << BLUE << "[TESTING ENDED]" << RESET << std::endl;
+    std::cout << "TEST RESULTS " << correct_counter_testUtils << " / " << test_counter_testUtils-1 << std::endl;
+    std::cout << CYAN << "************************************************************************************" << RESET << std::endl;
+}
+
+} 
+
+#endif 
--- tests/unit_tests/Reference_Frame_1_Unit_Test.cpp
+++ tests/unit_tests/Reference_Frame_1_Unit_Test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "Testing_Utils.h"
 #include "Reference_Frame.h"
+#include <Eigen/Dense>
+
 
 /*
 This test will confirm some basic functionality of the ReferenceFrame class. Below is a list
@@ -16,50 +19,99 @@ Verify that the transformation matrix calc works
 
 */
 
-int main() {
-    std::cout << "UNIT TEST *STARTED*" << std::endl;
-    std::cout << std::endl << "TEST 1" << std::endl;
-    std::cout << "EXPECTED: " << std::endl 
-    << "RFA angles: " << "0, 0, 0" << std::endl
-    << "RFB angles: " << "4, 5, 6" << std::endl;
+
+
+/*
+    Tests to make sure constructor is setting the angles properly for default constructor
+*/
+void test_1() {
+
+    Eigen::Vector3d compare_1(0,0,0);
 
     ReferenceFrame reference_frame_A;
-    ReferenceFrame reference_frame_B(1, 2, 3, 4, 5, 6);
 
-    std::cout << std::endl << "RETURNED: " << std::endl; 
-    std::cout << "RFA angles: " << reference_frame_A.get_angles() << std::endl;
-    std::cout << "RFB angles: " << reference_frame_B.get_angles() << std::endl;
+    TestUtils::pass_or_fail_printout(TestUtils::check_vector_3_similarity(compare_1, reference_frame_A.get_angles()));
+}
 
+/*
+    Tests to make sure constructor is setting the angles properly for passing values
+*/
+void test_2() {
 
+    Eigen::Vector3d compare_1(4, 5, 6);
 
-    std::cout << std::endl << "TEST 2" << std::endl;
-    std::cout << "EXPECTED: " << std::endl 
-    << "RFA place: " << "0, 0, 0" << std::endl
-    << "RFB place: " << "1, 2, 3" << std::endl;
+    ReferenceFrame reference_frame_A(1, 2, 3, 4, 5, 6);
 
-    std::cout << std::endl << "RETURNED: " << std::endl; 
-    std::cout << "RFA place: " << reference_frame_A.get_placement_vector() << std::endl;
-    std::cout << "RFB place: " << reference_frame_B.get_placement_vector() << std::endl;
+    TestUtils::pass_or_fail_printout(TestUtils::check_vector_3_similarity(compare_1, reference_frame_A.get_angles()));
+}
 
+/*
+    Tests to make sure constructor is setting the placement vector properly for default constructor
+*/
+void test_3() {
 
+    Eigen::Vector3d compare_1(0,0,0);
 
-    std::cout << std::endl << "TEST 3" << std::endl;
-    std::cout << "EXPECTED: " << std::endl 
-    << "RFA trans: " << "1, 0, 0, 0, \n 0, 1, 0, 0, \n 0, 0, 1, 0, \n 0, 0, 0, 1" << std::endl
-    << "RFB trans: " << "0.27, 0.51, 0.81, 1, \n -0.08, -0.83, 0.55, 2, \n 0.96, -0.21, -0.19, 3, \n 0, 0, 0, 1" << std::endl;
+    ReferenceFrame reference_frame_A;
 
-    std::cout << std::endl << "RETURNED: " << std::endl; 
-    std::cout << "RFA trans: " << reference_frame_A.get_transformation_matrix() << std::endl;
-    std::cout << "RFB trans: " << reference_frame_B.get_transformation_matrix() << std::endl;
+    TestUtils::pass_or_fail_printout(TestUtils::check_vector_3_similarity(compare_1, reference_frame_A.get_placement_vector()));
+}
 
+/*
+    Tests to make sure constructor is setting the placement vector properly for passing values
+*/
+void test_4() {
 
+    Eigen::Vector3d compare_1(1,2,3);
 
-    std::cout << std::endl << "TEST 4" << std::endl;
-    std::cout << "EXPECTED: " << std::endl 
-    << "RFA angles: " << "7, 8, 9" << std::endl
-    << "RFA place: " << "10, 11, 12" << std::endl
-    << "RFA trans: " << "0.13, -0.9, -0.41, 10, \n -0.06, -0.42, 0.91, 11, \n -0.99, -0.1, -0.11, 12, \n 0, 0, 0, 1" << std::endl;
+    ReferenceFrame reference_frame_A(1, 2, 3, 4, 5, 6);
 
+    TestUtils::pass_or_fail_printout(TestUtils::check_vector_3_similarity(compare_1, reference_frame_A.get_placement_vector()));
+
+}
+
+/*
+    Test to make sure the transformation matrix of the default reference frame is correct
+*/
+void test_5() {
+
+    Eigen::Matrix4d compare_1;
+    compare_1 << 1, 0, 0, 0,
+                 0, 1, 0, 0, 
+                 0, 0, 1, 0, 
+                 0, 0, 0, 1;
+
+    ReferenceFrame reference_frame_A;
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_4_similarity(compare_1, reference_frame_A.get_transformation_matrix()));
+
+}
+
+/*
+    Test to make sure the transformation matrix of the reference frame is correct
+*/
+void test_6() { 
+
+    Eigen::Matrix4d compare_1;
+    compare_1 << 0.27, 0.51, 0.81, 1,
+                 -0.08, -0.83, 0.55, 2,
+                 0.96, -0.21, -0.19, 3, 
+                 0,    0,     0,     1;
+
+    ReferenceFrame reference_frame_A(1, 2, 3, 4, 5, 6);
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_4_similarity(compare_1, reference_frame_A.get_transformation_matrix()));
+
+}
+
+/*
+   Test setters, check to see that angles is correct
+*/
+void test_7() { 
+
+    Eigen::Vector3d compare_1(7,8,9);
+
+    ReferenceFrame reference_frame_A;
 
     reference_frame_A.set_ax(7);
     reference_frame_A.set_ay(8);
@@ -68,47 +120,159 @@ int main() {
     reference_frame_A.set_oy(11);
     reference_frame_A.set_oz(12);
 
-    std::cout << std::endl << "RETURNED: " << std::endl; 
-    std::cout << "RFA angles: " << reference_frame_A.get_angles() << std::endl;
-    std::cout << "RFA place: " << reference_frame_A.get_placement_vector() << std::endl;
-    std::cout << "RFA trans: " << reference_frame_A.get_transformation_matrix() << std::endl;
+    TestUtils::pass_or_fail_printout(TestUtils::check_vector_3_similarity(compare_1, reference_frame_A.get_angles()));
+
+}
+
+/*
+   Test setters, check to see that placement is correct
+*/
+void test_8() { 
+
+    Eigen::Vector3d compare_1(10,11,12);
+
+    ReferenceFrame reference_frame_A;
+
+    reference_frame_A.set_ax(7);
+    reference_frame_A.set_ay(8);
+    reference_frame_A.set_az(9);
+    reference_frame_A.set_ox(10);
+    reference_frame_A.set_oy(11);
+    reference_frame_A.set_oz(12);
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_vector_3_similarity(compare_1, reference_frame_A.get_placement_vector()));
+
+}
+
+/*
+   Test setters, check to see that transformation is correct
+*/
+void test_9() { 
+
+    Eigen::Matrix4d compare_1;
+    compare_1 << 0.13, -0.9, -0.41, 10,
+                 -0.06, -0.42, 0.91, 11,
+                 -0.99, -0.1, -0.11, 12, 
+                 0,     0,    0,     1;
+
+    ReferenceFrame reference_frame_A;
+
+    reference_frame_A.set_ax(7);
+    reference_frame_A.set_ay(8);
+    reference_frame_A.set_az(9);
+    reference_frame_A.set_ox(10);
+    reference_frame_A.set_oy(11);
+    reference_frame_A.set_oz(12);
 
 
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_4_similarity(compare_1, reference_frame_A.get_transformation_matrix()));
 
-    std::cout << std::endl << "TEST 5" << std::endl;
-    std::cout << "EXPECTED: " << std::endl 
-    << "rot x: " << "1, 0, 0, \n 0, -0.99, -0.14, \n 0, 0.14, -0.99" << std::endl
-    << "rot y: " << "-0.99, 0, 0.14, \n 0, 1, 0, \n -0.14, 0, -0.99" << std::endl
-    << "rot z: " << "-0.99, -0.14, 0, \n 0.14, -0.99, 0, \n 0, 0, 1" << std::endl;
+}
 
-    std::cout << std::endl << "RETURNED: " << std::endl; 
-    std::cout << "rot x: " << MathPositionObject::calculate_rotation_matrix_x(3) << std::endl;
-    std::cout << "rot y: " << MathPositionObject::calculate_rotation_matrix_y(3) << std::endl;
-    std::cout << "rot z: " << MathPositionObject::calculate_rotation_matrix_z(3) << std::endl;
+/*
+   This tests the fidelity of the X axis rotation matrix 
+*/
+void test_10() { 
+
+    Eigen::Matrix3d compare_1;
+    compare_1 << 1, 0, 0, 
+                 0, -0.99, -0.14, 
+                 0, 0.14, -0.99;
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_3_similarity(compare_1,  MathPositionObject::calculate_rotation_matrix_x(3)));
+
+}
+
+/*
+   This tests the fidelity of the Y axis rotation matrix 
+*/
+void test_11() { 
+
+    Eigen::Matrix3d compare_1;
+    compare_1 << -0.99, 0, 0.14,
+                 0, 1, 0, 
+                 -0.14, 0, -0.99;
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_3_similarity(compare_1,  MathPositionObject::calculate_rotation_matrix_y(3)));
+
+}
+
+/*
+   This tests the fidelity of the Z axis rotation matrix 
+*/
+void test_12() { 
+
+    Eigen::Matrix3d compare_1;
+    compare_1 << -0.99, -0.14, 0,
+                 0.14, -0.99, 0, 
+                 0, 0, 1;
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_3_similarity(compare_1,  MathPositionObject::calculate_rotation_matrix_z(3)));
+
+}
+
+/*
+    This tests the fidelity of the transformation matrix calulation
+*/
+void test_13() { 
+
+    Eigen::Matrix4d compare_1;
+    compare_1 << -0.1, -0.91, -0.41, 16,
+                 0.09, -0.42, 0.9,   17, 
+                 -0.99, 0.06, 0.12,  18,
+                 0,     0,    0,     1;
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_4_similarity(compare_1, MathPositionObject::calculate_transformation_matrix(Eigen::Vector3d(13, 14, 15), Eigen::Vector3d(16, 17, 18))));
+
+}
+
+/*
+    This tests the fidelity of the reference frame expression
+*/
+void test_14() { 
+
+    Eigen::Matrix4d compare_1;
+    compare_1 << -0.91, -0.30, -0.29, 10.36,
+                 0.33, -0.09, -0.94, -4.77,
+                 0.26, -0.94, 0.19, 10.62,
+                 0,     0,    0,     1;
+
+    ReferenceFrame reference_frame_A;
+    ReferenceFrame reference_frame_B(1, 2, 3, 4, 5, 6);
+
+    reference_frame_A.set_ax(7);
+    reference_frame_A.set_ay(8);
+    reference_frame_A.set_az(9);
+    reference_frame_A.set_ox(10);
+    reference_frame_A.set_oy(11);
+    reference_frame_A.set_oz(12);
+
+    TestUtils::pass_or_fail_printout(TestUtils::check_matrix_4_similarity(compare_1, reference_frame_A.get_expressed_in(&reference_frame_B)->get_transformation_matrix()));
+
+}
 
 
+int main() {
 
+    TestUtils::start_testing();
 
-    std::cout << std::endl << "TEST 6" << std::endl;
-    std::cout << "EXPECTED: " << std::endl 
-    << "trans: " << "-0.1, -0.91, -0.41, 16, \n 0.09, -0.42, 0.9, 17, \n -0.99, 0.06, 0.12, 18, \n 0, 0, 0, 1" << std::endl;
+    TestUtils::perform_test(test_1);
+    TestUtils::perform_test(test_2);
+    TestUtils::perform_test(test_3);
+    TestUtils::perform_test(test_4);
+    TestUtils::perform_test(test_5);
+    TestUtils::perform_test(test_6);
+    TestUtils::perform_test(test_7);
+    TestUtils::perform_test(test_8);
+    TestUtils::perform_test(test_9);
+    TestUtils::perform_test(test_10);
+    TestUtils::perform_test(test_11);
+    TestUtils::perform_test(test_12);
+    TestUtils::perform_test(test_13);
+    TestUtils::perform_test(test_14);
 
+    TestUtils::end_testing();
 
-    std::cout << std::endl << "RETURNED: " << std::endl; 
-    std::cout << "trans: " << MathPositionObject::calculate_transformation_matrix(Eigen::Vector3d(13, 14, 15), Eigen::Vector3d(16, 17, 18)) << std::endl;
-
-
-
-    std::cout << std::endl << "TEST 7" << std::endl;
-    std::cout << "EXPECTED: " << std::endl 
-    << "trans: " << "-0.91, -0.30, -0.29, 10.36, \n 0.33, -0.09, -0.94, -4.77, \n 0.26, -0.94, 0.19, 10.62, \n 0, 0, 0, 1" << std::endl;
-
-
-    std::cout << std::endl << "RETURNED: " << std::endl; 
-    std::cout << "trans: " << reference_frame_A.get_expressed_in(&reference_frame_B)->get_transformation_matrix() << std::endl;
-
-
-    std::cout << "UNIT TEST *COMPLETED*" << std::endl;
     return 0;
 }
 
