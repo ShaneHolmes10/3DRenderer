d12274021549145636c53399ce04c6f1b13f2518
shaneholmes10 <shaneholmes2010@gmail.com>
2026-02-01 14:33:52 -0500

    finalized model, comments and test

---
+++ 3DRenderer/representations/model.cpp
@@ -0,0 +1,21 @@
+
+#include "representations/model.h"
+#include "utils/load_obj_file.h" // Should really use a generic loader in the future
+
+Model::Model(
+        const std::vector<Mesh>& meshes
+    ) : meshes(meshes)
+{
+
+}
+
+void Model::loadMesh(const std::string& filepath) {
+
+    LoadObjFile loader;
+    meshes.push_back(loader.load(filepath));
+
+}
+
+const std::vector<Mesh>& Model::getMeshes() const {
+    return meshes;
+}
