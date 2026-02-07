33afdbb98a6585a81cb296e9ca25c252c8984e53
shaneholmes10 <shaneholmes2010@gmail.com>
2026-02-07 17:38:49 -0500

    add error handling for illegal pixel level access

--- 3DRenderer/display/frame_buffer.cpp
+++ 3DRenderer/display/frame_buffer.cpp
@@ -5,6 +5,11 @@ FrameBuffer::FrameBuffer(size_t w, size_t h)
     : width(w), height(h), pixels(w * h * 3, 0) {}
 
 void FrameBuffer::setPixel(size_t x, size_t y, uint8_t r, uint8_t g, uint8_t b) {
+    
+    if (x >= width || y >= height) {
+        throw std::out_of_range("Pixel coordinates out of bounds");
+    }
+
     size_t index = (y * width + x) * 3;
     pixels[index + 0] = r;
     pixels[index + 1] = g;
