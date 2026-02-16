5e57a54b6299599d2a95b38e8788b1e416afe245
shaneholmes10 <shaneholmes2010@gmail.com>
2026-02-16 12:28:26 -0500

    added a way to load a cobj file which includes vertex color information. This isn't a standard obj framework this is just a hacky thing to show color

---
+++ 3DRenderer/utils/load_cobj_file.cpp
@@ -0,0 +1,43 @@
+#include "load_cobj_file.h"
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+Mesh LoadCobjFile::load(const std::string& filepath) {
+    std::vector<Vertex> vertices;
+    std::vector<Face> faces;
+    
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file: " + filepath);
+    }
+    
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string prefix;
+        iss >> prefix;
+        
+        if (prefix == "v") {
+            Vertex vertex;
+            iss >> vertex.position.x() >> vertex.position.y() >> vertex.position.z();
+            
+            float r, g, b;
+            if (iss >> r >> g >> b) {
+                vertex.color = Eigen::Vector3i(r * 255, g * 255, b * 255);
+            }
+            
+            vertices.push_back(vertex);
+        }
+        else if (prefix == "f") {
+            Face face;
+            iss >> face.v1 >> face.v2 >> face.v3;
+            face.v1--;
+            face.v2--;
+            face.v3--;
+            faces.push_back(face);
+        }
+    }
+    
+    return Mesh(vertices, faces);
+}
