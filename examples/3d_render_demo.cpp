21550ff6a89b4361151d02d0d9a02555f170668f
shaneholmes10 <shaneholmes2010@gmail.com>
2026-02-16 11:53:38 -0500

    Created a demo file to demo the rasterization pipeline

--- examples/3d_render_demo.cpp
+++ examples/3d_render_demo.cpp
@@ -8,6 +8,8 @@
 #include <Eigen/Dense>
 #include <iostream>
 #include <cmath>
+#include <thread>
+#include <chrono>
 
 int main() {
     
@@ -17,55 +19,66 @@ int main() {
     int height = 600;
     Viewport view(width, height, 100, 100);
     
-    // Create world root entity
     Entity world;
     
-    // ==== Create tetrahedron entity ====
     Entity tetrahedron_entity;
-
     Model tetrahedron_model;
     std::string filename = std::string(SRC_DIR) + "/data/tetrahedron.obj";
     tetrahedron_model.loadMesh(filename);
-
     tetrahedron_entity.model = &tetrahedron_model;
     
-    Transform t_tetra(Eigen::Vector3f(0, 0, 0), Eigen::Vector3f::Zero());
+    Transform t_tetra(Eigen::Vector3f(0, 0, 0), Eigen::Vector3f::Zero(), Eigen::Vector3f(150, 150, 150));
     tetrahedron_entity.setTransform(t_tetra);
     
     world.addChild(tetrahedron_entity);
     
-    // ==== Create camera mount entity ====
     Entity camera_mount;
-    Transform t_cam(Eigen::Vector3f(0, 50, -300), Eigen::Vector3f::Zero());
-    std::cout << "t_cam world coordinate " << t_cam.getMatrix() << "\n";
-    camera_mount.setTransform(t_cam);
-    
     world.addChild(camera_mount);
     
-    // ==== Create camera ====
     Camera camera;
     camera.attachTo(camera_mount);
-    camera.setFovLength(200);
+    camera.setFovLength(150);
     camera.setPictureWidthHeight(width, height);
     
-    // Render
-    FrameBuffer frame_buffer(width, height);
-    
-    DrawCommand cmd;
-    cmd.entity = &tetrahedron_entity;
-    cmd.cull_mode = CullMode::None;
-    
-    camera.draw(frame_buffer, cmd);
-    
-    // Display
     view.start();
-    view.setFrame(frame_buffer);
-    view.update();
     
-    std::cout << "Tetrahedron rendered. Close window to exit.\n";
-    std::cout << "Root world coordinate " << world.getWorldMatrix() << "\n";
-    std::cout << "Tetra world coordinate " << tetrahedron_entity.getWorldMatrix() << "\n";
-    std::cout << "Camera world coordinate " << camera_mount.getWorldMatrix() << "\n";
+    // Animation loop - orbit camera around tetrahedron
+    float angle = 0;
+    while(true) {
+        
+        float orbit_radius = 300.0f;
+        float theta = angle * 0.05f;
+        
+        // Camera position (orbit in circle)
+        float cam_x = orbit_radius * std::sin(theta);
+        float cam_y = 50.0f;
+        float cam_z = orbit_radius * std::cos(theta);
+        
+        // Camera rotation (look at origin where tetrahedron is)
+        // Rotate around Y axis to face the center
+        float rotation_y = theta + M_PI;  // Face inward toward origin
+        
+        Transform t_cam(Eigen::Vector3f(cam_x, cam_y, cam_z), 
+                        Eigen::Vector3f(0, rotation_y, 0));
+        camera_mount.setTransform(t_cam);
+        
+        FrameBuffer frame_buffer(width, height);
+        
+        DrawCommand cmd;
+        cmd.entity = &tetrahedron_entity;
+        cmd.cull_mode = CullMode::None;
+        
+        camera.draw(frame_buffer, cmd);
+        
+        view.setFrame(frame_buffer);
+        view.update();
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    
+        angle += 1.0f;
+    }
+    
+    std::cout << "Animation complete. Close window to exit.\n";
     
     view.join();
     
