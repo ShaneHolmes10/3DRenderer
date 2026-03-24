4d73f8d317917142572c1208a909b3ad136ec538
shaneholmes10 <shaneholmes2010@gmail.com>
2026-03-24 14:28:58 -0400

    Added key press bindings and callback event handling

---
+++ 3DRenderer/display/key.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+enum KeyCode {
+    A = sf::Keyboard::A,
+    B = sf::Keyboard::B,
+    C = sf::Keyboard::C,
+    D = sf::Keyboard::D,
+    E = sf::Keyboard::E,
+    F = sf::Keyboard::F,
+    G = sf::Keyboard::G,
+    H = sf::Keyboard::H,
+    I = sf::Keyboard::I,
+    J = sf::Keyboard::J,
+    K = sf::Keyboard::K,
+    L = sf::Keyboard::L,
+    M = sf::Keyboard::M,
+    N = sf::Keyboard::N,
+    O = sf::Keyboard::O,
+    P = sf::Keyboard::P,
+    Q = sf::Keyboard::Q,
+    R = sf::Keyboard::R,
+    S = sf::Keyboard::S,
+    T = sf::Keyboard::T,
+    U = sf::Keyboard::U,
+    V = sf::Keyboard::V,
+    W = sf::Keyboard::W,
+    X = sf::Keyboard::X,
+    Y = sf::Keyboard::Y,
+    Z = sf::Keyboard::Z,
+    
+    Num0 = sf::Keyboard::Num0,
+    Num1 = sf::Keyboard::Num1,
+    Num2 = sf::Keyboard::Num2,
+    Num3 = sf::Keyboard::Num3,
+    Num4 = sf::Keyboard::Num4,
+    Num5 = sf::Keyboard::Num5,
+    Num6 = sf::Keyboard::Num6,
+    Num7 = sf::Keyboard::Num7,
+    Num8 = sf::Keyboard::Num8,
+    Num9 = sf::Keyboard::Num9,
+    
+    Escape = sf::Keyboard::Escape,
+    Space = sf::Keyboard::Space,
+    Enter = sf::Keyboard::Enter,
+    Tab = sf::Keyboard::Tab,
+    
+    Up = sf::Keyboard::Up,
+    Down = sf::Keyboard::Down,
+    Left = sf::Keyboard::Left,
+    Right = sf::Keyboard::Right,
+    
+    F1 = sf::Keyboard::F1,
+    F2 = sf::Keyboard::F2,
+    F3 = sf::Keyboard::F3,
+    F4 = sf::Keyboard::F4,
+    F5 = sf::Keyboard::F5,
+    F6 = sf::Keyboard::F6,
+    F7 = sf::Keyboard::F7,
+    F8 = sf::Keyboard::F8,
+    F9 = sf::Keyboard::F9,
+    F10 = sf::Keyboard::F10,
+    F11 = sf::Keyboard::F11,
+    F12 = sf::Keyboard::F12,
+    
+    Unknown = sf::Keyboard::Unknown
+};
